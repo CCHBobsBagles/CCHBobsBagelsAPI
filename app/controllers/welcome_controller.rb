e83925335d6b66d8fae68fd9e190a28fe53e4810
raielin@gmail.com <raielin@gmail.com>
2014-11-10 11:42:11 -0500

    Resolved merge conflict

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,7 @@
 class WelcomeController < ApplicationController
 
 def index
-<<<<<<< HEAD
+
   render text: "WECOME!"
-=======
-  render: "welcome/index"
->>>>>>> ea0c7cbdba6331e50e67324314399d9b26555a43
-end
 
 end
