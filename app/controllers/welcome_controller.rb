ef29a5603a9da20ea5aaba7208c00d48a29b3bd1
raielin@gmail.com <raielin@gmail.com>
2014-11-10 12:32:13 -0500

    fixing unmerged changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,12 @@
 class WelcomeController < ApplicationController
 
 def index
+<<<<<<< HEAD
   render text: "WECOME!"
 end
+=======
+
+  render text: "WECOME!"
+>>>>>>> e839253... Resolved merge conflict
 
 end
